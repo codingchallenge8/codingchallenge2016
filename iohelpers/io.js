36b267e43dd4b663c35b0a742482dc4b58205806
codingchallenge8 <coding.challenge@yandex.com>
2016-08-11 10:18:26 +0800

    Create io.js

---
+++ iohelpers/io.js
@@ -0,0 +1,6 @@
+var line;
+while (line = readline()) {
+    var nums = line.split(' '),
+    var a = parseInt(nums[0]),
+    var b = parseInt(nums[1]);
+    /*Solve the test case and output the answer*/
